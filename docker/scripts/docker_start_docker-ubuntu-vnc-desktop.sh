0b7f58c0057ffb4a7acd1b45baf3c9ee89e57af9
mayel <mayel.espino@gmail.com>
2024-10-04 20:30:34 +0000

    added jenkins and ubuntu-vnc scripts.

---
+++ docker/scripts/docker_start_docker-ubuntu-vnc-desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker run --name ubvnc -p 6080:80 -p 5900:5900 dorowu/ubuntu-desktop-lxde-vnc:bionic
--- docker/scripts/docker_start_jenkins.sh
+++ docker/scripts/docker_start_jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo docker run -d --privileged -p 8000:8080 --name jenkins-1 --mount type=bind,source="/home/mayel/GIT",target=/home jenkins/jenkins:lts-jdk11
+#sudo docker run -d --privileged -p 8000:8080 --name jenkins-1 --mount type=bind,source="/home/mayel/GIT",target=/home jenkins/jenkins:lts-jdk11
+sudo docker run --add-host host.local.internal:host-gateway -d --privileged -p 8000:8080 --name jenkins-1 --mount type=bind,source="/home/mayel/",target=/var/jenkins_home/ jenkins/jenkins
 
 echo "Now running:"
 sudo docker ps
